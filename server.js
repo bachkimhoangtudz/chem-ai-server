dc7a3543e9652ca51ad4279ae731a6ceaa0805e1
bachkimhoangtudz <bachkimhoangtudz@gmail.com>
2026-05-11 20:58:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const OpenAI = require("openai");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
 app.use(express.json());
 
 const client = new OpenAI({
