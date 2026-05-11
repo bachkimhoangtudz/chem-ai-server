20d413546ce0d29eb05b29da1c63b0219aefd9ec
bachkimhoangtudz <bachkimhoangtudz@gmail.com>
2026-05-11 20:51:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,61 @@
 const express = require("express");
+const cors = require("cors");
+const OpenAI = require("openai");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("SERVER WORKING");
+app.use(cors());
+app.use(express.json());
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Server OK");
+});
+
+app.post("/chat", async (req, res) => {
+
+    try {
+
+        const userMessage = req.body.message;
+
+        const completion =
+            await client.chat.completions.create({
+
+            model: "gpt-4.1-mini",
+
+            messages: [
+                {
+                    role: "system",
+                    content: "Bạn là ChemAI."
+                },
+                {
+                    role: "user",
+                    content: userMessage
+                }
+            ]
+        });
+
+        res.json({
+            reply:
+                completion.choices[0]
+                .message.content
+        });
+
+    } catch (err) {
+
+        console.log(err);
+
+        res.status(500).json({
+            reply: "AI Error"
+        });
+    }
+});
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log("Server running on " + PORT);
+    console.log("Running on " + PORT);
 });
